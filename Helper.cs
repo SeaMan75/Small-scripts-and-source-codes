d372cfdae68e055f1f40b7fe473c8c630e91ff46
SeaMan75 <bgnick@mail.ru>
2024-12-20 21:01:56 +0300

    Add files via upload

--- Helper.cs
+++ Helper.cs
@@ -49,8 +49,20 @@ namespace main
 			public int Right;
 			public int Bottom;
 		}
-
-
+		
+		public static bool MoveWindowToPosition(int pos_x, int pos_y, int size_x, int size_y, ProgramInfo programInfo) {
+			bool result = true;
+			try {
+				var hwnd = wnd.find(1, programInfo.Title, programInfo.ClassName, programInfo.Owner).Activate();
+				// Перемещаем окно в заданную позицию и устанавливаем размеры
+				hwnd.Move(pos_x, pos_y, size_x, size_y);
+			}
+			catch (Exception ex) {
+				lastErrorMessage = ex.Message;
+				result = false;
+			}
+			return result;
+		}
 		public static bool MoveWindowLeft(int dx, int dy, ProgramInfo programInfo)
 		{
 			int screenWidth;
@@ -292,52 +304,68 @@ namespace main
 			const int WM_NCLBUTTONDOWN = 0xA1;
 			const int HTCAPTION = 0x2;
 
-			const string TOTAL_COMMANDER_PATH = @"C:\Total Commander Extended\Totalcmd64.exe";
+			const string OBSIDIAN_PATH = @"C:\Program Files\Obsidian\Obsidian.exe";
+			const string OBSIDIAN_CLASS_NAME= @"Chrome_WidgetWin_1";
+			const string OBSIDIAN_TITLE= @"*Obsidian*";
+			const string OBSIDIAN_OWNER= @"Obsidian.exe";
 			
 			const string OPERA_PATH = @"C:\Users\bgnic\AppData\Local\Programs\Opera\opera.exe";
 			const string OPERA_CLASS_NAME = @"Chrome_WidgetWin_1";
 			const string OPERA_TITLE = @"*";
+			const string OPERA_OWNER = @"opera.exe";
 			
 			const string TODO_PATH = @"C:\Users\bgnic\AppData\Local\Swift\To-Do List\Swift To-Do List.exe";
 			const string WORD_PATH = @"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE";
+			const string WORD_CLASS_NAME = @"OpusApp";
+			const string WORD_TITLE = @"*Word";
+			const string WORD_OWNER = "WINWORD.EXE";
 			
 			const string PYCHARM_PATH = @"C:\Program Files\JetBrains\PyCharm2024.3\bin\pycharm64.exe";
 			const string SUBLIME_PATH = @"C:\Program Files\Sublime Text\sublime_text.exe";
+			
 			const string NETBEANS_PATH = @"C:\Program Files\NetBeans-23\netbeans\bin\netbeans64.exe";
+			const string NETBEANS_CLASS_NAME= @"SunAwtFrame";
+			const string NETBEANS_TITLE= @"*Apache NetBeans*";
+			const string NETBEANS_OWNER= @"netbeans64.exe";
 			
 			const string VISUAL_STUDIO_CODE_TITLE = @"*Visual Studio Code";
 			const string VISUAL_STUDIO_CLASS_NAME = @"Chrome_WidgetWin_1";	
-			const string VISUAL_STUDIO_PROGRAM = @"Code.exe";	
+			const string VISUAL_STUDIO_OWNER = @"Code.exe";	
 
+			const string TOTAL_COMMANDER_PATH = @"C:\Total Commander Extended\Totalcmd64.exe";
 			const string TOTAL_COMMANDER_TITLE = "*Total Commander (x64) 11.03*";
 			const string TOTAL_COMMANDER_CLASS_NAME = "TTOTAL_CMD";
 			
 			const string VISUAL_STUDIO_CODE_PATH = @"C:\Users\bgnic\AppData\Local\Programs\Microsoft VS Code\Code.exe";
 			const string VISUAL_STUDIO_CODE_HEADER = "* Visual Studio Code";
 			const string VISUAL_STUDIO_CODE_CLASS_NAME = "Chrome_WidgetWin_1";
-			const string VISUAL_STUDIO_CODE_PROGRAM = "Code.exe";
+			const string VISUAL_STUDIO_CODE_OWNER = "Code.exe";
 			
 			const string SKYPE_PATH = @"C:\Program Files\WindowsApps\Microsoft.SkypeApp_15.132.3201.0_x64__kzf8qxf38zg5c\Skype\Skype.exe";
-			const string SKYPE_TITLE = "Skype";
+			const string SKYPE_TITLE = "Skype*";
 			const string SKYPE_CLASS_NAME = "Chrome_WidgetWin_1";
 			const string SKYPE_PROGRAM = "Skype.exe";
 
-			const string SWIFT_TO_DO_LIST_HEADER = "Swift To-Do List 11*";
+			const string SWIFT_TO_DO_LIST_PATH = @"C:\Users\bgnic\AppData\Local\Swift To-Do List\Swift To-Do List.exe";
+			const string SWIFT_TO_DO_LIST_TITLE = "Swift To-Do List 11*";
 			const string SWIFT_TO_DO_LIST_CLASS_NAME = "*.Window.*;";
-			const string SWIFT_TO_DO_LIST_PROGRAM = "";
+			const string SWIFT_TO_DO_LIST_OWNER = "";
 			
 			const string FAR_MANAGER_PATH = @"C:\Program Files\ConEmu\ConEmu64.exe";
 			const string FAR_MANAGER_TITLE = @"*Far*";
 			const string FAR_MANAGER_CLASS_NAME = @"VirtualConsoleClass"; 
-			const string FAR_MANAGER_PROGRAM = @"ConEmu64.exe";	
+			const string FAR_MANAGER_OWNER = @"ConEmu64.exe";
 			
-			
+			const string EMUL_PATH = @"K:\VK_Projects\Василий Воронин\Assembler 80\(1)";
+			const string EMUL_TITLE = @"Модель лабораторного стенда УМПК-80";
+			const string EMUL_CLASS_NAME = @"TLForm1"; 	
+			const string EMUL_OWNER = @"K580BM80.EXE";
 			static void Main()
 			{
 				script.setup(trayIcon: true, sleepExit: true);
 				var form = new Form();
 				form.Text = "Helper";
-				form.Size = new DrawingSize(400, 450);
+				form.Size = new DrawingSize(650, 450);
 				form.TopMost = true;
 
 				form.Load += (sender, e) => form.Location = new DrawingPoint(3000, 1600);
@@ -361,12 +389,27 @@ namespace main
 				var tabPage2 = new TabPage("Вкладка 2");
 				var tabPage3 = new TabPage("Вкладка 1");
 				var tabPage4 = new TabPage("Вкладка 2");
-
+				
+				var textBox1 = new TextBox();
+				textBox1.Multiline = true;
+				textBox1.Width = 350;
+				textBox1.Dock = DockStyle.Right;
+				textBox1.ScrollBars = ScrollBars.Both;
+				
+				var textBox2 = new TextBox();
+				textBox2.Multiline = true;
+				textBox2.Width = 400;
+				textBox2.Dock = DockStyle.Right;
+				textBox2.ScrollBars = ScrollBars.Both;
+				
 				// Добавляем вкладки в TabControl
 				tabControl.TabPages.Add(tabPage1);
 				tabControl.TabPages.Add(tabPage2);
 				tabControl.TabPages.Add(tabPage3);
 				tabControl.TabPages.Add(tabPage4);
+				
+				tabPage1.Controls.Add(textBox1);
+				tabPage2.Controls.Add(textBox2);
 
 				//			AddMouseDownHandler(form, form);
 				AddMouseDownHandler(tabPage1, form);
@@ -409,7 +452,24 @@ namespace main
 				var button11 =
 					CreateButton(tabPage2, "Opera - FarManager", new DrawingPoint(10, 50),
 								 (sender, e) => FindWindow11());
-				
+				var button12 =
+					CreateButton(tabPage2, "Skype - Word", new DrawingPoint(10, 90),
+								 (sender, e) => FindWindow12());
+				var button13 =
+					CreateButton(tabPage2, "Word - Far Manager", new DrawingPoint(10, 130),
+								 (sender, e) => FindWindow13());
+				var button14 =
+					CreateButton(tabPage2, "Emulator", new DrawingPoint(10, 170),
+								 (sender, e) => FindWindow14());
+				var button15 =
+					CreateButton(tabPage2, "TODO - MS Word", new DrawingPoint(10, 210),
+								 (sender, e) => FindWindow15());
+				var button16 =
+					CreateButton(tabPage2, "Opera - MS Word", new DrawingPoint(10, 250),
+								 (sender, e) => FindWindow16());
+				var button17 =
+					CreateButton(tabPage2, "Obsaidni - Net Beans", new DrawingPoint(10, 290),
+								 (sender, e) => FindWindow17());
 
 				form.ShowDialog();
 			}
@@ -468,7 +528,7 @@ namespace main
 					dx: 512,
 					dy: 128,
 					path: (TODO_PATH, TOTAL_COMMANDER_PATH),
-					name1: SWIFT_TO_DO_LIST_HEADER,
+					name1: SWIFT_TO_DO_LIST_TITLE,
 					cn1: SWIFT_TO_DO_LIST_CLASS_NAME,
 					name2: TOTAL_COMMANDER_TITLE,
 					cn2: TOTAL_COMMANDER_CLASS_NAME,
@@ -480,11 +540,12 @@ namespace main
 			static void FindWindow3()
 			{
 
-				FindAndLocateWindow(
-					path: WORD_PATH,
-					name: "*Word",
-					cn: "OpusApp"
-				);
+				var word = new ProgramInfo(
+						path: WORD_PATH
+						, title: WORD_TITLE
+						, className: WORD_CLASS_NAME
+						, owner: WORD_OWNER);
+				FindAndLocateWindow(word);
 			}
 
 			static void FindWindow4()
@@ -577,7 +638,7 @@ namespace main
 						, className: VISUAL_STUDIO_CODE_CLASS_NAME);
 				
 				FindAndLocateWindows(
-				dx: 512, dy: 128, (skype, vscode));
+				dx: 128, dy: 128, (skype, vscode));
 			}
 			
 			static void FindWindow11()
@@ -589,13 +650,113 @@ namespace main
 				var opera = new ProgramInfo(
 						path: OPERA_PATH
 						, title: OPERA_TITLE
-						, className: OPERA_CLASS_NAME);
+						, className: OPERA_CLASS_NAME
+						, owner: OPERA_OWNER);
 				
 				FindAndLocateWindows(
-				dx: 512, dy: 128, (opera, far_manager));
+				dx: 128, dy: 128, (opera, far_manager));
+			}
+			
+			static void FindWindow12()
+			{
+				var skype = new ProgramInfo(
+						path: SKYPE_PATH
+						, title: SKYPE_TITLE
+						, className: SKYPE_CLASS_NAME);
+				var word = new ProgramInfo(
+						path: WORD_PATH
+						, title: WORD_TITLE
+						, className: WORD_CLASS_NAME
+						, owner: WORD_OWNER);
+				
+				FindAndLocateWindows(
+				dx: 128, dy: 128, (skype, word));
 			}
 
+			static void FindWindow13()
+			{
+				var far_manager = new ProgramInfo(
+						path: FAR_MANAGER_PATH
+						, title: FAR_MANAGER_TITLE
+						, className: FAR_MANAGER_CLASS_NAME);
+				var word = new ProgramInfo(
+						path: WORD_PATH
+						, title: WORD_TITLE
+						, className: WORD_CLASS_NAME
+						, owner: WORD_OWNER);
+				
+				FindAndLocateWindows(
+				dx: 128, dy: 128, (word, far_manager));
+			}
+			static void FindWindow14()
+			{
+				var emul = new ProgramInfo(
+						path: EMUL_PATH
+						, title: EMUL_TITLE
+						, className: EMUL_CLASS_NAME);
+				var word = new ProgramInfo(
+						path: WORD_PATH
+						, title: WORD_TITLE
+						, className: WORD_CLASS_NAME
+						, owner: WORD_OWNER);
+				var opera = new ProgramInfo(
+						path: OPERA_PATH
+						, title: OPERA_TITLE
+						, className: OPERA_CLASS_NAME
+						, owner: OPERA_OWNER);
+				
+				MoveWindowToPosition(pos_x: 108, pos_y: 1050, size_x: 1088, size_y: 990, emul);
+				MoveWindowRight(dx: 12, dy: 128, word);
+				MoveWindowToPosition(pos_x: 30, pos_y: 30, size_x: 1800, size_y: 1000, opera);
+			}
+			static void FindWindow15()
+			{
+				var todo = new ProgramInfo(
+						path: SWIFT_TO_DO_LIST_PATH
+						, title: SWIFT_TO_DO_LIST_TITLE
+						, className: SWIFT_TO_DO_LIST_CLASS_NAME);
+				var word = new ProgramInfo(
+						path: WORD_PATH
+						, title: WORD_TITLE
+						, className: WORD_CLASS_NAME
+						, owner: WORD_OWNER);
+				
+				FindAndLocateWindows(
+				dx: 128, dy: 128, (todo, word));
+			}
+			static void FindWindow16()
+			{
+				var opera = new ProgramInfo(
+						path: OPERA_PATH
+						, title: OPERA_TITLE
+						, className: OPERA_CLASS_NAME
+						, owner: OPERA_OWNER);
+				var word = new ProgramInfo(
+						path: WORD_PATH
+						, title: WORD_TITLE
+						, className: WORD_CLASS_NAME
+						, owner: WORD_OWNER);
+				
+				FindAndLocateWindows(
+				dx: 128, dy: 128, (opera, word));
+			}
 			
+			static void FindWindow17()
+			{
+				var obsidian = new ProgramInfo(
+						path: OBSIDIAN_PATH
+						, title: OBSIDIAN_TITLE
+						, className: OBSIDIAN_CLASS_NAME
+						, owner: OBSIDIAN_OWNER);
+				var netbeans = new ProgramInfo(
+						path: NETBEANS_PATH
+						, title: NETBEANS_TITLE
+						, className: NETBEANS_CLASS_NAME
+						, owner: NETBEANS_OWNER);
+				
+				FindAndLocateWindows(
+				dx: 128, dy: 128, (obsidian, netbeans));
+			}
 			
 			static void Click1()
 			{
